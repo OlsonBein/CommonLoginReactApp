1fc41e76f932029676ad13fb77c3b50945cbbbaf
Oleksii Morozov <oleksii.morozov@nixsolutions.com>
2021-02-10 12:29:15 +0200

    Add closed endpoint. Change header

---
+++ CommonLoginReactApp/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CommonLoginReactApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : Controller
+    {
+        [HttpGet("closedEndpoint")]
+        public IActionResult Index()
+        {
+            return this.Ok("Congratulations, you got here!");
+        }
+    }
+}
